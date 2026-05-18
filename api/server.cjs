4435de9851da1f92608c2d99918617d09f78156f
Peterson William <petersondev.profissional@gmail.com>
2026-05-18 20:53:14 -0300

    update

--- api/server.cjs
+++ api/server.cjs
@@ -1,9 +1,15 @@
 const jsonServer = require('json-server');
 const path = require('path');
 
+const dbPath = path.join(__dirname, '..', 'db.json');
+
+console.log('db path:', dbPath);
+console.log('dirname:', __dirname);
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const router = jsonServer.router(dbPath);
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(router);
 
